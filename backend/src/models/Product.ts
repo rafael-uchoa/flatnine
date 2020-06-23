366333b384b2e9770549ecfc5e6b89a27b80a308
Rafael Uchoa <rafaeluchoa.1996@gmail.com>
2020-06-23 14:56:15 -0300

    Added image upload support

--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -4,6 +4,7 @@ export interface IProduct extends Document {
   name: string;
   price: number;
   category: string;
+  imageUrl: string;
   createdAt: Date;
 }
 
@@ -11,6 +12,7 @@ const ProductSchema: Schema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   category: { type: String, required: true },
+  imageUrl: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
